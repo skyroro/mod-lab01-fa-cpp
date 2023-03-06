82abd9403d356b90c3e24d02029fb1918286c704
skyroro <126852495+skyroro@users.noreply.github.com>
2023-03-07 01:00:34 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,64 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+  int count = 0;
+  int countWithNumber = 0;
+  bool isWord = false;
+  bool wordWithNumber = false;
+  while(*str) {
+    if (*str >= '0' && *str <= '9' && wordWithNumber == false) {
+        countWithNumber++;
+        wordWithNumber = true;
+    }
+    if (*str != ' ' && isWord == false) {
+      isWord = true;
+      count++;
+    } else if (*str == ' ' && isWord == true) {
+      isWord = false;
+      wordWithNumber = false;
+    }
+    ++str;
+  }
+  return (count - countWithNumber);
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+  char *str ="Hello world!";
+  int count = 0;
+  int isOurWord = 0;
+  while (*str) {
+    if (*str >= 'A' && *str <= 'Z' && isOurWord == 0) {
+      isOurWord = 1;
+    } else if (*str >= 'a' && *str <= 'z' && isOurWord == 1) {
+      isOurWord = 1;
+    } else if (*str != ' ' && isOurWord == 1){
+      isOurWord = -1;
+    } else if (*str == ' ' && isOurWord == 1) {
+      count++;
+      isOurWord = 0;
+    } else if (*str == ' ') {
+      isOurWord = 0;
+    }
+    ++str;
+  }
+  return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+  int countWord = 0;
+  int countLetters = 0;
+  bool isWord = false;
+  while (*str) {
+    if (*str != ' ' && isWord == false) {
+      countWord++;
+      countLetters++;
+      isWord = true;
+    } else if (*str != ' ') {
+      countLetters++;
+    } else if (*str == ' ' && isWord == true) {
+      isWord = false;
+    }
+    ++str;
+  }
+  return (countLetters/countWord);
 }
